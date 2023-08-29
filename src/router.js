8549e26df13d8cecd9c1b0b4307fede467c5dc0d
Gleb Fokin <g.fokin@skyeng.ru>
2023-08-29 12:45:16 +0700

    Добавялет basename

--- src/router.js
+++ src/router.js
@@ -2,13 +2,16 @@ import { createBrowserRouter } from "react-router-dom";
 import { GamePage } from "./pages/GamePage/GamePage";
 import { SelectLevelPage } from "./pages/SelectLevelPage/SelectLevelPage";
 
-export const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <SelectLevelPage />,
-  },
-  {
-    path: "/game/:pairsCount",
-    element: <GamePage />,
-  },
-]);
+export const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <SelectLevelPage />,
+    },
+    {
+      path: "/game/:pairsCount",
+      element: <GamePage />,
+    },
+  ],
+  { basename: "/react-memo" },
+);
